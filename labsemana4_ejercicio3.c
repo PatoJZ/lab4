a73be917831502116ab6517dfa36d0a352390726
PatoJZ <patojimenez08@gmail.com>
2023-04-06 22:51:50 -0400

    06-04

--- labsemana4_ejercicio3.c
+++ labsemana4_ejercicio3.c
@@ -6,7 +6,7 @@ typedef struct {
     char name[50];
     int age;
     int room;
-    float temperature;
+    float temperature[3];
 } Patient;
 
 void addPatient(Patient arr[], int* NumPatients){
@@ -18,14 +18,24 @@ void addPatient(Patient arr[], int* NumPatients){
         scanf("%d",&(arr[*NumPatients].age));
         printf("Introduce la habitacion del paciente %d: ",*NumPatients+1);
         scanf("%d",&(arr[*NumPatients].room));
-        printf("Introduce la temperatura del paciente %d: ",*NumPatients+1);
-        scanf("%f",&(arr[*NumPatients].temperature));
     }
     (*NumPatients)++;
 }
 void printPatient(Patient arr[], int max){
-    printf(" --------\n Nombre: %s\n Edad: %d\n Habitacion: %d\n Temperatura: %2f\n", arr[max].name, arr[max].age, arr[max].room, arr[max].temperature);
- 
+    printf(" --------\n Nombre: %s\n Edad: %d\n Habitacion: %d\n ", arr[max].name, arr[max].age, arr[max].room);
+    for (int i = 0; i < 3; i++){
+        printf("Temperatura %d: %f\n", i+1, arr[max].temperature[i]);
+    }
+}
+void recordTemperatures(Patient arr[], int max){
+    int i;
+    for (i = 0; i < max; i++){
+        for(int j = 0; j < 3; j++){
+            printf("--------Temperatura %d--------\n",j+1);
+            printf("Introduce la temperatura del paciente %d: ",i+1);
+            scanf("%f",&(arr[i].temperature[j]));
+        }
+    }
 }
 int main(){
     Patient pacientes[MAX_GlOBAL];
@@ -34,11 +44,10 @@ int main(){
     printf("Ingrese el numero de pacientes a atender: ");
     scanf("%d",&temp);
     
-    
     for(int i=0;i<temp;i++){
         addPatient(pacientes, &NumPatients);
     }
-    
+    recordTemperatures(pacientes, temp);
 
     for(int i=0; i<temp; i++){
         printPatient(pacientes,i);
